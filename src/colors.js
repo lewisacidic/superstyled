edec42c35c5cf18e14cda52895c012b788d6a5dc
Rich Lewis <richlewis42@users.noreply.github.com>
2018-12-04 14:06:06 +0000

    feat(colors): add colors property

---
+++ src/colors.js
@@ -0,0 +1,13 @@
+import { style } from '.'
+
+export const color = style({
+  prop: '$color',
+  css: 'color',
+  key: 'colors'
+})
+
+export const background = style({
+  prop: '$background',
+  css: 'background',
+  key: 'backgrounds'
+})
