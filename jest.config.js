68f1dd9fc2eae01b53f1824f6549fe81e66acec1
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-01-14 11:32:17 +0000

    build: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,31 @@
 module.exports = {
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    "^.+\\.js$": "babel-jest"
   },
-  testRegex: '.+\\.spec\\.js$',
-  testPathIgnorePatterns: ['node_modules', 'dist'],
-  moduleFileExtensions: ['js'],
-  testURL: 'http://localhost',
-  setupTestFrameworkScriptFile: '<rootDir>/src/__tests__/test.config.js',
+  transformIgnorePatterns: ["<rootDir>/node_modules"],
+  testRegex: ".+\\.spec\\.js$",
+  testPathIgnorePatterns: ["node_modules", "dist"],
+  moduleFileExtensions: ["js"],
+  testURL: "http://localhost",
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/test.config.js"],
   reporters: [
-    'default',
+    "default",
     [
-      'jest-html-reporter',
+      "jest-html-reporter",
       {
-        outputPath: 'reports/html/unit-tests.html',
-        theme: 'lightTheme'
+        outputPath: "reports/html/unit-tests.html",
+        theme: "lightTheme"
       }
     ],
     [
-      'jest-junit',
+      "jest-junit",
       {
-        output: 'reports/junit/unit-tests.xml'
+        outputName: "reports/junit/unit-tests.xml"
       }
     ]
   ],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['text', 'html'],
-  coverageDirectory: 'reports/coverage'
+  collectCoverageFrom: ["src/**/*.js"],
+  coverageReporters: ["text", "html"],
+  coverageDirectory: "reports/coverage"
 }
