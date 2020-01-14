a37f1e295599fd1cd831613d7749fc36d9ec7106
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-01-14 11:30:39 +0000

    build: updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,56 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        modules: 'auto',
-        useBuiltIns: 'usage',
-        shippedProposals: true,
-        targets: {
-          browsers: ['>0.25%', 'not dead']
+const path = require("path")
+const resolve = require.resolve
+
+module.exports = (api, options = {}) => {
+  api.cache(true)
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          loose: true,
+          modules: "auto",
+          useBuiltIns: "usage",
+          shippedProposals: true,
+          targets: {
+            browsers: [">0.25%", "not dead"]
+          },
+          exclude: ["transform-typeof-symbol"]
         }
-      }
+      ],
+      [
+        resolve("@babel/preset-react"),
+        {
+          useBuiltIns: true,
+          pragma: "React.createElement",
+          development: false
+        }
+      ]
+    ],
+    plugins: [
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          loose: true
+        }
+      ],
+      resolve("@babel/plugin-proposal-nullish-coalescing-operator"),
+      resolve("@babel/plugin-proposal-optional-chaining"),
+      resolve("babel-plugin-macros"),
+      resolve("@babel/plugin-syntax-dynamic-import"),
+      [
+        resolve("@babel/plugin-transform-runtime"),
+        {
+          corejs: false,
+          helpers: options.stage === "develop" || options.stage === "test",
+          regenerator: true,
+          useESModules: options.state !== "test",
+          absoluteRuntimePath: path.dirname(
+            resolve("@babel/runtime/package.json")
+          )
+        }
+      ],
+      resolve("@babel/plugin-transform-spread")
     ]
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true
-      }
-    ],
-    '@babel/plugin-syntax-dynamic-import',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        helpers: true,
-        regenerator: true
-      }
-    ],
-    '@babel/plugin-proposal-optional-chaining',
-    'babel-plugin-lodash'
-  ]
+  }
 }
