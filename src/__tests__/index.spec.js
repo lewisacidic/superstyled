30d1b882f3affe69eb2ba0775430274ce031859c
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-01-15 00:42:49 +0000

    fix: remove polished test

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,7 +1,6 @@
 import React from "react"
 import TestRenderer from "react-test-renderer"
 
-import { lighten } from "polished"
 import { color } from ".."
 
 it("should work", () => {
@@ -23,20 +22,6 @@ it("should work with functions", () => {
     <p
       css={color("red")
         .hover(c => "brick" + c)
-        .done()}
-    >
-      wow
-    </p>
-  ).toJSON()
-
-  expect(tree).toMatchSnapshot()
-})
-
-it("should work with pigs", () => {
-  const tree = TestRenderer.create(
-    <p
-      css={color("red")
-        .hover(lighten(0.1))
         .done()}
     >
       wow
