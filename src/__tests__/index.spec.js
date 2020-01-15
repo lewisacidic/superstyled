49028f80c70f18edd6aaef6f40b51ca23101fc7f
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2020-01-15 01:01:56 +0000

    test: add more tests of basic functionality

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -3,30 +3,70 @@ import TestRenderer from "react-test-renderer"
 
 import { color } from ".."
 
-it("should work", () => {
-  const tree = TestRenderer.create(
+const render = jsx => TestRenderer.create(jsx).toJSON()
+
+it("should add styles", () => {
+  const tree = render(<p css={color("red").done()}>test content</p>)
+  expect(tree).toHaveStyleRule("color", "red")
+  expect(tree).toMatchSnapshot()
+})
+
+it("should add hover rules with `.hover`", () => {
+  const tree = render(
     <p
       css={color("red")
         .hover("green")
         .done()}
     >
-      wow
+      test content
     </p>
-  ).toJSON()
+  )
 
+  expect(tree).toHaveStyleRule("color", "red")
+  expect(tree).toHaveStyleRule("color", "green", { target: ":hover" })
   expect(tree).toMatchSnapshot()
 })
 
+it("should add active rules with `.active`", () => {
+  const tree = render(
+    <button
+      css={color("red")
+        .active("blue")
+        .done()}
+    >
+      test content
+    </button>
+  )
+
+  expect(tree).toHaveStyleRule("color", "red")
+  expect(tree).toHaveStyleRule("color", "blue", { target: ":active" })
+  expect(tree).toMatchSnapshot()
+})
+
+it("should add arbitrary rules with `.on`", () => {
+  const tree = render(
+    <div
+      css={color("red")
+        .on("::before", "yellow")
+        .done()}
+    />
+  )
+  expect(tree).toHaveStyleRule("color", "red")
+  expect(tree).toHaveStyleRule("color", "yellow", { target: "::before" })
+  expect(tree).toMatchSnapshot()
+})
 it("should work with functions", () => {
-  const tree = TestRenderer.create(
+  const tree = render(
     <p
       css={color("red")
         .hover(c => "brick" + c)
         .done()}
     >
-      wow
+      test content
     </p>
-  ).toJSON()
+  )
 
+  expect(tree).toHaveStyleRule("color", "red")
+  expect(tree).toHaveStyleRule("color", "brickred", { target: ":hover" })
   expect(tree).toMatchSnapshot()
 })
